91530567890a5542d6faa6ba9620d912f4bd6e0d
Gene Grady <grady.gene.thomas@gmail.com>
2019-11-12 10:03:42 -0500

    Controllers and routes set up

--- app/controllers/padders_controller.rb
+++ app/controllers/padders_controller.rb
@@ -1,2 +1,56 @@
 class PaddersController < ApplicationController
+    before_action :set_padder, only: [:show, :edit, :update, :delete]
+    def index
+        @padders = Padder.all
+    end
+
+    def show
+    end
+
+    def new
+        @padder = Padder.new
+        @errors = flash[:errors]
+    end
+
+    def create
+        @padder= Padder.create(strong_params(:username, :password, :bio, :languages, :zodiac, :location_id, :countries_visited, :room_available))
+
+        if @padder.valid?
+            redirect_to @padder
+        else
+            flash[:errors]= @padder.errors.full_messages
+            redirect_to new_padder_path
+        end
+    end
+
+    def edit
+        @errors = flash[:errors]
+    end
+
+    def update
+        Padder.update(strong_params(:username, :password, :bio, :languages, :zodiac, :location_id, :countries_visited, :room_available))
+
+        if @padder.valid?
+            redirect_to @padder
+        else
+            flash[:errors]= @padder.errors.full_messages
+            redirect_to edit_padder_path
+        end
+    end
+
+    def delete
+        @padder.destroy
+        redirect_to padders_path
+    end
+
+    private
+
+    def strong_params(*args)
+        params.require(:padder).permit(*args)
+    end
+
+    def set_padder
+        @padder = Padder.find(params[:id])
+    end
+
 end
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,28 @@
 class RequestsController < ApplicationController
+
+    def new
+        @request = Request.new
+        @errors = flash[:errors]
+    end
+
+    def create
+        @request = Request.create(strong_params)
+
+        if @request.valid?
+            redirect_to request_path(@request.requester_padder_id)
+        else
+            flash[:errors]= @request.errors.full_messages
+            redirect_to new_request_path
+        end
+    end
+
+    def show
+        @request = Request.find(params[:id])
+    end
+
+    private
+
+    def strong_params
+        params.require(:request).permit(:requestee_padder_id,:requester_padder_id )
+    end
 end
